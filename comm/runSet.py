d1ee9e19f8af7378f13c750de445ebefd85a7373
wmengchen <15051968801@163.com>
2020-07-15 10:39:36 +0800

    指定某个文件夹批量运行

--- comm/runSet.py
+++ comm/runSet.py
@@ -4,5 +4,51 @@
 # @name: runSet
 # @author：menghuan.wmc
 import os,sys
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+proDir = os.path.split(os.path.dirname(__file__))[0]
+
+
 import unittest
+filepath = ''
+
+def set_case_list():
+    caseList = []
+    caseListPath = os.path.join(proDir,'caseList.txt')
+    fb = open(caseListPath)
+    for case in fb.readlines():
+        data = str(case)
+        if data != ""and not data.startswith('#'):
+            caseList.append(data.replace("\n",""))
+    fb.close()
+    return caseList
+
+def set_suite():
+    global filepath
+    suite_list = unittest.TestSuite()
+    suite_module = []
+    case_list = set_case_list()
+    for case in case_list:
+        name = str(case).split('/')[-1]
+        module = str(case).split('/')[0]
+        filepath = os.path.join(proDir,'case',module)
+        # print('filepath:',filepath)
+        discover = unittest.defaultTestLoader.discover(filepath,pattern=name +'.py',top_level_dir=filepath)
+        suite_module.append(discover)
+        # print(suite_module)
+
+    if len(suite_module)>0:
+        for case in suite_module:
+            for name in case:
+                # print('name:',name)
+                suite_list.addTests(name)
+    else:
+        return None
+    return suite_list
+
+if __name__=="__main__":
+    set_suite()
+
+
+
+
+
+
--- runAll.py
+++ runAll.py
@@ -1,25 +1,20 @@
-# !/usr/bin/python
-# -*- coding: UTF-8 -*-
-# @date: 2020/4/14 11:27
-# @name: setting
-# @author：menghuan.wmc
-
-
-import os, sys
-
+# # !/usr/bin/python
+# # -*- coding: UTF-8 -*-
+# # @date: 2020/4/14 11:27
+# # @name: setting
+# # @author：menghuan.wmc
+#
+#
+import os, sys,time
 sys.path.append(os.path.dirname(__file__))
-pro_path = os.path.dirname(__file__)
-import time
-import unittest
+print('os:',os.path.dirname(__file__))
+
 from config import setting
 from package.HTMLTestRunner import HTMLTestRunner
+from comm.runSet import set_suite
+import HTMLTestRunner_PY3
 
 
-
-
-test_path = setting.Test_case_execute
-second_dir_path = os.listdir(test_path)
-print('second_dir_path:',second_dir_path)
 result_path = setting.Test_report
 
 
@@ -28,46 +23,15 @@ def run_case(result_path=setting.Test_report):
 
     now = time.strftime("%Y-%m-%d %H_%M")
     filename = result_path + '/' + now + ' result.html'
-    fp = open(filename, 'wb')
-    #加入case列表
-    dir1 = "case/project"
-    dir2 = "case/dataSource"
-    test_dirs = list()
-    test_dirs.append(dir1)
-    test_dirs.append(dir2)
+    fp = open(filename,'wb')
 
+    suite = set_suite()
+    print('suite:',suite)
+    runner = HTMLTestRunner_PY3.HTMLTestRunner(stream=fp, title='虎符自动化测试报告', description='测试用例结果')
 
-
-    suite = unittest.TestSuite()
-    for item in test_dirs:
-        discover = unittest.defaultTestLoader.discover(start_dir=item,pattern='*test.py',top_level_dir=None)
-        for case1 in discover:
-            for case in case1:
-                suite.addTests(case)
-
-        runner = HTMLTestRunner(stream=fp, title='虎符接口自动化测试报告',
-                                description='环境：windows 10 浏览器：chrome',
-                                tester='menghuan')
-
-        runner.run(suite)
-
-    # case_path = os.path.join(pro_path, 'case')
-    # print('case_path:',case_path)
-    # list_case = ['project','dataSource']
-    # suite = unittest.TestSuite()
-    #
-    # for i in range(len(list_case)):
-    #
-    #     discover = unittest.defaultTestLoader.discover(case_path+"/" +list_case[i], pattern='*_test.py', top_level_dir=None)
-
+    runner.run(suite)
+    fp.close()
 
 
 if __name__ == "__main__":
     run_case()
-
-
-
-
-
-
-
