4094c7e5ae862bba5ac959a1b74ac24eee454639
wmengchen <15051968801@163.com>
2020-07-20 13:32:26 +0800

    增加滚动条

--- comm/login.py
+++ comm/login.py
@@ -15,7 +15,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException,TimeoutException
 from comm.element import Element
-
+from selenium.webdriver.common.keys import Keys
 #读取配置文件
 cf = cparser.ConfigParser()
 cf.read(setting.Test_config,encoding='utf-8')
@@ -60,46 +60,27 @@ class Login():
         k = WebDriverWait(self.browser,10).until(EC.element_to_be_clickable((By.XPATH,'//button[@type="submit"]')))
         # print('k',k)
         k.click()
-        WebDriverWait(self.browser, 10).until_not(EC.element_to_be_clickable((By.XPATH, '//button[@type="submit"]')))
 
 
 
 
-        # self.browser.find_element_by_xpath('//button[@type="submit"]').click()
-        # sleep(3)
-
-
-        # try:
+        #单元测试验证具体页面
+        # sleep(1)
+        # self.browser.find_element_by_xpath('//*[@id="container"]/section/section/section/main/div/div[1]/span/input').send_keys('2020-07-20_wmc_edit')
+        # sleep(1)
+        # self.browser.find_element_by_xpath('//*[@id="container"]/section/section/section/main/div/div[1]/span/input').send_keys(Keys.ENTER)
+        # sleep(2)
+        # self.browser.find_element_by_xpath('//p[@class="item-title"]').click()
+        # sleep(1)
+        # self.browser.find_element_by_xpath('//li/a[text()="数据资产"]').click()
+        # self.browser.find_element_by_xpath('//*[@id="container"]/section/section/section/main/div/p/button').click()
+        # sleep(2)
+        # js = "document.getElementsByClassName('add-dataSourde')[0].scrollTop=1000"
+        # self.browser.execute_script(js)
         #
-        #     y = WebDriverWait(self.browser,10).until(EC.element_to_be_clickable((By.XPATH,'//button[@class = "ant-btn ant-btn-primary"]')))
-        #     y.click()
-        #     t = WebDriverWait(self.browser,10).until(EC.visibility_of_element_located((By.XPATH,'//input[@placeholder="请输入项目名称"]')))
-        #     t.send_keys('ceshixuyao1112311')
-        #     y = WebDriverWait(self.browser, 10).until(EC.visibility_of_element_located((By.XPATH, '//textarea[@placeholder="请输入描述"]')))
-        #     y.send_keys('ceshixuyao1112311')
-        # except (NoSuchElementException,TimeoutException)as e:
-        #     raise e
-        # self.browser.find_element_by_xpath('//button[@class = "ant-btn ant-btn-primary"]').click()
-        # self.browser.find_element_by_xpath('//input[@placeholder="请输入项目名称"]').send_keys('ceshixuyao1112311')
-        # self.browser.find_element_by_xpath('//textarea[@placeholder="请输入描述"]').send_keys('nihao')
-        # self.browser.find_element_by_xpath('/html/body/div[2]/div/div[2]/div/div[2]/div[3]/div/button[1]').click()
-        # sleep(1)
-        # self.browser.find_element_by_xpath('//button[@class = "ant-btn ant-btn-primary"]').click()
-        # self.browser.find_element_by_xpath('//input[@placeholder="请输入项目名称"]').send_keys('ceshixuyao1112311')
-        # self.browser.find_element_by_xpath('//textarea[@placeholder="请输入描述"]').send_keys('nihao')
-        # sleep(1)
-        # try:
-        #     t = '/html/body/div[2]/div/div[2]/div/div[2]/div[3]/div/button[2]'
-        #     et = WebDriverWait(self.browser,10).until(EC.presence_of_element_located(By.XPATH),t)
-        #     et.click()
-        # finally:
-        #     sleep(1)
+        # sleep(10)
 
-        # self.browser.find_element_by_xpath('/html/body/div[2]/div/div[2]/div/div[2]/div[3]/div/button[2]').click()
-        # sleep(9)
-        # # nihao = WebDriverWait(browser,10).until(EC.element_to_be_clickable((By.XPATH,'/div/div/div/span')))
-        # print(nihao)
-        # sleep(1)
+
     def logout(self):
         self.browser.close()
 
