ce79ef6ba17c673a32145dd22ce1d55c8431c7e8
wmengchen <15051968801@163.com>
2020-09-07 13:36:37 +0800

    针对异常case执行下一步的时候，每一步骤的url都是一样的，可以通过能否进行下一步的属性值做判断，要不要执行下一步

--- case/dataTable/guideCreateTable_ads.py
+++ case/dataTable/guideCreateTable_ads.py
@@ -1,23 +1,23 @@
-#!/usr/bin/python
+# !/usr/bin/python
 # -*- coding: UTF-8 -*-
-# @date: 2020/7/24 10:21 
+# @date: 2020/7/24 10:21
 # @name: guideCreateTable
 # @author：menghuan.wmc
 
-import ddt,unittest,sys,os,traceback
+import ddt, unittest, sys, os, time
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
-from comm.element import  Element
+from comm.element import Element
 from comm.readExcel import ReadExcel
 from comm import login
 from config import setting
 from selenium.webdriver.common.keys import Keys
-from comm.sql import Dbconnect
-import time
-from selenium.webdriver.common.action_chains import ActionChains
+
+
 
 sheetName = 'guideCreateTable_ads'
-date = time.strftime('%Y_%m_%d',time.localtime(time.time()))
-testData = ReadExcel(setting.Test_case,sheetName).read_data()
+date = time.strftime('%Y_%m_%d', time.localtime(time.time()))
+testData = ReadExcel(setting.Test_case, sheetName).read_data()
+
 
 @ddt.ddt
 class guideCreateTable(unittest.TestCase):
@@ -30,31 +30,33 @@ class guideCreateTable(unittest.TestCase):
         pass
 
     @ddt.data(*testData)
-    def test_guideCreateTable(self,data):
+    def test_guideCreateTable(self, data):
 
         print('---------{}---------'.format(data['case_name']))
 
-        Element(self.driver,'project','Projectfind_click').wait_send_keys(date+data["project_name"])
-        Element(self.driver,'project','Projectfind_click').send_keys(Keys.ENTER)
+        Element(self.driver, 'project', 'Projectfind_click').wait_send_keys(date + data["project_name"])
+        Element(self.driver, 'project', 'Projectfind_click').send_keys(Keys.ENTER)
         time.sleep(1)
-        Element(self.driver,'project','enterProject_click').wait_click()
+        Element(self.driver, 'project', 'enterProject_click').wait_click()
         time.sleep(1)
-        Element(self.driver,'dataStanard','dataStanard_click').wait_click()
+        Element(self.driver, 'dataStanard', 'dataStanard_click').wait_click()
         Element(self.driver, 'dataStanard', 'modelDesign_click').wait_click()
-        Element(self.driver,'dataStanard', 'guideCreateTable_click').wait_click()
+        Element(self.driver, 'dataStanard', 'guideCreateTable_click').wait_click()
         Element(self.driver, 'dataStanard', 'levelchoose_click').wait_click()
         Element(self.driver, 'dataStanard', 'levelchoose_ads_click').wait_click()
-        Element(self.driver,'dataStanard','apply_choose_click').wait_click()
-        Element(self.driver,'dataStanard','applychoose_ai_click').wait_click()
-        Element(self.driver,'dataStanard','define_click').wait_send_keys(data["define"])
+        Element(self.driver, 'dataStanard', 'apply_choose_click').wait_click()
+        Element(self.driver, 'dataStanard', 'applychoose_ai_click').wait_click()
+        Element(self.driver, 'dataStanard', 'define_click').wait_send_keys(data["define"])
         Element(self.driver, 'dataStanard', 'tabledesc_click').wait_send_keys(data["desc"])
+        Element(self.driver, 'dataStanard', 'tabledesc_info').wait_click()
+        time.sleep(1)
+        value = Element(self.driver, 'dataStanard', 'tabledesc_info').get_attribute(data["property"])
+        print('value的值是：', value)
         Element(self.driver, 'dataStanard', 'tablelifeStyle_click').wait_click()
-        Element(self.driver,'dataStanard','tablelifeStyle_select').wait_click()
-        Element(self.driver,'dataStanard','tablelifeStyle_input').wait_send_keys(int(data["lifecycle"]))
+        Element(self.driver, 'dataStanard', 'tablelifeStyle_select').wait_click()
+        Element(self.driver, 'dataStanard', 'tablelifeStyle_input').wait_send_keys(int(data["lifecycle"]))
         Element(self.driver, 'dataStanard', 'next_click').wait_click()
-        values= Element(self.driver,'dataStanard', 'next_click').get_attribute(data["property"])
-        print('value的值是：',values)
-        if values == 'true':
+        if value == 'correct':
             Element(self.driver, 'dataStanard', 'addpar_click').wait_click()
             Element(self.driver, 'dataStanard', 'paraname1_click').wait_click()
             Element(self.driver, 'dataStanard', 'paraname1_inputclick').wait_send_keys(data["para1name"])
@@ -130,10 +132,9 @@ class guideCreateTable(unittest.TestCase):
         else:
             pass
 
-
     @ddt.data(testData)
-    def check_result(self,url):
-        if int[testData["result"]]==0:
+    def check_result(self, url):
+        if int[testData["result"]] == 0:
             assert url == testData['expect_url']
         else:
             assert url != testData['expect_url']
@@ -142,5 +143,7 @@ class guideCreateTable(unittest.TestCase):
         print('--------测试结束--------')
         self.login.logout()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     unittest.main()
+
